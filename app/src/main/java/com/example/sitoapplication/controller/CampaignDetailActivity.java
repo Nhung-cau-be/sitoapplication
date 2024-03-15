ecd116e76607750478888304ca7f0f326f14a6ac
Lê Anh Quân <anhquanle2015@gmail.com>
2024-03-15 15:42:00 +0700

    save

---
+++ app/src/main/java/com/example/sitoapplication/controller/CampaignDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.sitoapplication.controller;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CampaignDetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/sitoapplication/controller/ListCampaignActivity.java
+++ app/src/main/java/com/example/sitoapplication/controller/ListCampaignActivity.java
@@ -1,21 +1,14 @@
 package com.example.sitoapplication.controller;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ListView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.sitoapplication.R;
 import com.example.sitoapplication.controller.adapter.CampaignArrayAdapter;
-import com.example.sitoapplication.entity.Campaign;
 import com.example.sitoapplication.model.CampaignViewModel;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ListCampaignActivity extends AppCompatActivity {
     private CampaignViewModel campaignViewModel;
@@ -32,7 +25,7 @@ public class ListCampaignActivity extends AppCompatActivity {
         listViewCampaign = findViewById(R.id.lvChienDich);
         campaignViewModel.getAll().observe(this, campaigns ->  {
             // Update the cached copy of the words in the adapter.
-            campaignArrayAdapter = new CampaignArrayAdapter(this, R.layout.item, campaigns);
+            campaignArrayAdapter = new CampaignArrayAdapter(this, R.layout.list_campaign_item, campaigns);
             listViewCampaign.setAdapter(campaignArrayAdapter);
         });
     }
