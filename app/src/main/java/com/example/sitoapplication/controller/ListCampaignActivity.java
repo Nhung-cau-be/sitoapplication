27f536497d21dfdb1cadbece32faa755f5630154
ddoo177 <linhkhanh1164@gmail.com>
2024-03-21 17:22:46 +0700

    Sign up and store user in FireStore
    he he he

--- app/src/main/java/com/example/sitoapplication/controller/ListCampaignActivity.java
+++ app/src/main/java/com/example/sitoapplication/controller/ListCampaignActivity.java
@@ -1,6 +1,7 @@
 package com.example.sitoapplication.controller;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -11,6 +12,8 @@ import com.example.sitoapplication.R;
 import com.example.sitoapplication.adapter.CampaignArrayAdapter;
 import com.example.sitoapplication.model.CampaignViewModel;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ListCampaignActivity extends AppCompatActivity {
     private CampaignViewModel campaignViewModel;
@@ -18,31 +21,44 @@ public class ListCampaignActivity extends AppCompatActivity {
     private CampaignArrayAdapter campaignArrayAdapter;
     private MaterialToolbar topAppBar;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_campaign);
 
-        listViewCampaign = findViewById(R.id.lvChienDich);
-        topAppBar = findViewById(R.id.listCampaignTopAppBar);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null) {
+            String name = user.getDisplayName();
+            String phone = user.getPhoneNumber();
+            Uri url = user.getPhotoUrl();
+            String uid = user.getUid();
+            String email = user.getEmail();
 
-        campaignViewModel = new ViewModelProvider(this).get(CampaignViewModel.class);
+            System.out.println(name + phone + url +uid + email);
+        }
 
-        campaignViewModel.getAll().observe(this, campaigns ->  {
-            // Update the cached copy of the words in the adapter.
-            campaignArrayAdapter = new CampaignArrayAdapter(this, R.layout.list_campaign_item, campaigns);
-            listViewCampaign.setAdapter(campaignArrayAdapter);
-        });
 
-        topAppBar.setOnMenuItemClickListener(item -> {
-            if(item.getItemId() == R.id.lctab_create_campaign) {
-                Intent intent = new Intent(ListCampaignActivity.this, CreateCampaignActivity.class);
-                startActivity(intent);
-
-                return true;
-            }
-
-            return false;
-        });
+//        listViewCampaign = findViewById(R.id.lvChienDich);
+//        topAppBar = findViewById(R.id.listCampaignTopAppBar);
+//
+//        campaignViewModel = new ViewModelProvider(this).get(CampaignViewModel.class);
+//
+//        campaignViewModel.getAll().observe(this, campaigns ->  {
+//            // Update the cached copy of the words in the adapter.
+//            campaignArrayAdapter = new CampaignArrayAdapter(this, R.layout.list_campaign_item, campaigns);
+//            listViewCampaign.setAdapter(campaignArrayAdapter);
+//        });
+//
+//        topAppBar.setOnMenuItemClickListener(item -> {
+//            if(item.getItemId() == R.id.lctab_create_campaign) {
+//                Intent intent = new Intent(ListCampaignActivity.this, CreateCampaignActivity.class);
+//                startActivity(intent);
+//
+//                return true;
+//            }
+//
+//            return false;
+//        });
     }
 }
