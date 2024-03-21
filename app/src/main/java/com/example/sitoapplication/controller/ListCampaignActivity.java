9852f905f0e9c0b6ffe14fc689fb1c7f2ed4743d
ddoo177 <linhkhanh1164@gmail.com>
2024-03-21 17:28:41 +0700

    Revert "Sign up and store user in FireStore"
    This reverts commit 27f536497d21dfdb1cadbece32faa755f5630154.

--- app/src/main/java/com/example/sitoapplication/controller/ListCampaignActivity.java
+++ app/src/main/java/com/example/sitoapplication/controller/ListCampaignActivity.java
@@ -1,7 +1,6 @@
 package com.example.sitoapplication.controller;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -12,8 +11,6 @@ import com.example.sitoapplication.R;
 import com.example.sitoapplication.adapter.CampaignArrayAdapter;
 import com.example.sitoapplication.model.CampaignViewModel;
 import com.google.android.material.appbar.MaterialToolbar;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class ListCampaignActivity extends AppCompatActivity {
     private CampaignViewModel campaignViewModel;
@@ -21,44 +18,31 @@ public class ListCampaignActivity extends AppCompatActivity {
     private CampaignArrayAdapter campaignArrayAdapter;
     private MaterialToolbar topAppBar;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_campaign);
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user != null) {
-            String name = user.getDisplayName();
-            String phone = user.getPhoneNumber();
-            Uri url = user.getPhotoUrl();
-            String uid = user.getUid();
-            String email = user.getEmail();
+        listViewCampaign = findViewById(R.id.lvChienDich);
+        topAppBar = findViewById(R.id.listCampaignTopAppBar);
 
-            System.out.println(name + phone + url +uid + email);
-        }
+        campaignViewModel = new ViewModelProvider(this).get(CampaignViewModel.class);
 
+        campaignViewModel.getAll().observe(this, campaigns ->  {
+            // Update the cached copy of the words in the adapter.
+            campaignArrayAdapter = new CampaignArrayAdapter(this, R.layout.list_campaign_item, campaigns);
+            listViewCampaign.setAdapter(campaignArrayAdapter);
+        });
 
-//        listViewCampaign = findViewById(R.id.lvChienDich);
-//        topAppBar = findViewById(R.id.listCampaignTopAppBar);
-//
-//        campaignViewModel = new ViewModelProvider(this).get(CampaignViewModel.class);
-//
-//        campaignViewModel.getAll().observe(this, campaigns ->  {
-//            // Update the cached copy of the words in the adapter.
-//            campaignArrayAdapter = new CampaignArrayAdapter(this, R.layout.list_campaign_item, campaigns);
-//            listViewCampaign.setAdapter(campaignArrayAdapter);
-//        });
-//
-//        topAppBar.setOnMenuItemClickListener(item -> {
-//            if(item.getItemId() == R.id.lctab_create_campaign) {
-//                Intent intent = new Intent(ListCampaignActivity.this, CreateCampaignActivity.class);
-//                startActivity(intent);
-//
-//                return true;
-//            }
-//
-//            return false;
-//        });
+        topAppBar.setOnMenuItemClickListener(item -> {
+            if(item.getItemId() == R.id.lctab_create_campaign) {
+                Intent intent = new Intent(ListCampaignActivity.this, CreateCampaignActivity.class);
+                startActivity(intent);
+
+                return true;
+            }
+
+            return false;
+        });
     }
 }
