69f735940c69bb1cd1c4fc71c444a541c49b0647
ddoo177 <92584232+ddoo177@users.noreply.github.com>
2024-03-28 14:49:11 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/sitoapplication/MainActivity.java
+++ app/src/main/java/com/example/sitoapplication/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, CreateCampaignActivity.class);
+        Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
 }
